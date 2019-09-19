67c57f18b2fd1393180553bee99ed38631ba3ab3
Andres Sommariva <andres.sommariva@appdirect.com>
2019-09-19 14:57:46 -0300

    Streams solution.

--- new-in-java-8/src/main/java/com/exercises/java8/Streams.java
+++ new-in-java-8/src/main/java/com/exercises/java8/Streams.java
@@ -2,39 +2,50 @@ package com.exercises.java8;
 
 import java.awt.*;
 import java.util.List;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.stream.Collectors;
 
 public class Streams {
 
     public List<Point> getUniquePointsInQuadrantOne(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .filter(p -> p.x > 0 && p.y > 0)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Point> getUniquePointsInQuadrantTwo(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .filter(p -> p.x < 0 && p.y > 0)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Point> getUniquePointsInQuadrantThree(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .filter(p -> p.x < 0 && p.y < 0)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Point> getUniquePointsInQuadrantFour(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .filter(p -> p.x > 0 && p.y < 0)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     public List<Double> getSortedPointsDistanceToOrigin(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .mapToDouble(p -> Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2)))
+                .sorted()
+                .boxed()
+                .collect(Collectors.toList());
     }
 
     public Double getSumOfAllDistances(List<Point> points) {
-        // TODO: Implement this method
-        throw new NotImplementedException();
+        return points.stream()
+                .mapToDouble(p -> Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2)))
+                .reduce(0.0, (v1, v2) -> v1 + v2);
     }
 
 }
