1423a91a1317bcf705b985c65d096587bfea5d3d
Andres Sommariva <andres.sommariva@appdirect.com>
2019-10-02 12:27:44 -0300

    Optionals solution.

--- new-in-java-8/src/main/java/com/exercises/java8/Optionals.java
+++ new-in-java-8/src/main/java/com/exercises/java8/Optionals.java
@@ -1,12 +1,11 @@
 package com.exercises.java8;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import lombok.Builder;
 import lombok.Data;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Optionals {
 
@@ -58,7 +57,67 @@ public class Optionals {
      */
     public List<String> getPurchaseOrdersReportLines(PurchaseOrder purchaseOrder) {
         // TODO: Implement this method (see method javadoc for details)
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+
+        List<String> reportLines = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        // Header
+        sb.append("Buyer: ");
+        sb.append(getUserName(purchaseOrder.getBuyer()));
+        purchaseOrder.getSeller().ifPresent(user -> {
+            sb.append(" - Seller: ");
+            sb.append(getUserName(user));
+        });
+        reportLines.add(sb.toString());
+
+        // Items
+        purchaseOrder.getItems().stream()
+                .map(i -> getReportLine(i, purchaseOrder))
+                .forEach(r -> reportLines.add(r));
+
+        return reportLines;
+    }
+
+    private String getUserName(User user) {
+        return user.getFullName().orElse(user.getUserName());
+    }
+
+    private String getReportLine(PurchaseOrderItem purchaseOrderItem, PurchaseOrder purchaseOrder) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getProductDescription(purchaseOrderItem));
+        sb.append(" - ");
+        sb.append(getShippingDescription(purchaseOrderItem, purchaseOrder));
+        sb.append(" - Bill to: ");
+        sb.append(getBillingDescription(purchaseOrderItem, purchaseOrder));
+        return sb.toString();
+    }
+
+    private String getProductDescription(PurchaseOrderItem purchaseOrderItem) {
+        return purchaseOrderItem.getProductCode().orElse(purchaseOrderItem.getProductName());
+    }
+
+    private String getShippingDescription(PurchaseOrderItem purchaseOrderItem, PurchaseOrder purchaseOrder) {
+        StringBuilder sb = new StringBuilder();
+
+        if(purchaseOrderItem.isShippable()) {
+            sb.append("Ship to: ");
+            sb.append(formatAddress(purchaseOrder.getShippingAddress().orElse(purchaseOrder.getBuyer().getPersonalAddress())));
+        } else {
+            sb.append("NON-SHIPPABLE");
+        }
+
+        return sb.toString();
+    }
+
+    private String getBillingDescription(PurchaseOrderItem purchaseOrderItem, PurchaseOrder purchaseOrder) {
+        return Optional.of(purchaseOrder.getBillingAddress().orElse(purchaseOrder.getShippingAddress().orElse(purchaseOrder.getBuyer().getPersonalAddress())))
+                .map(this::formatAddress)
+                .get();
+    }
+
+    private String formatAddress(Address address) {
+        return String.format("%s, %s, %s, %s", address.getStreet(), address.getCity(), address.getPostalCode(), address.getCountry());
     }
 
 }
