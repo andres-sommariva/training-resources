67d58510989081377c9bcb0eccec8da627f7acf4
Mauro Casciati <mauro.casciati@appdirect.com>
2019-10-02 18:33:19 -0300

    Streams prettier

--- new-in-java-8/src/main/java/com/exercises/java8/Streams.java
+++ new-in-java-8/src/main/java/com/exercises/java8/Streams.java
@@ -2,36 +2,25 @@ package com.exercises.java8;
 
 import java.awt.*;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Streams {
 
     public List<Point> getUniquePointsInQuadrantOne(List<Point> points) {
-        return points.stream()
-          .filter(point -> point.getX() > 0 && point.getY() > 0)
-          .distinct()
-          .collect(Collectors.toList());
+        return pointFilter(points, point -> point.getX() > 0 && point.getY() > 0);
     }
 
     public List<Point> getUniquePointsInQuadrantTwo(List<Point> points) {
-        return points.stream()
-          .filter(point -> point.getX() < 0 && point.getY() > 0)
-          .distinct()
-          .collect(Collectors.toList());
+        return pointFilter(points, point -> point.getX() < 0 && point.getY() > 0);
     }
 
     public List<Point> getUniquePointsInQuadrantThree(List<Point> points) {
-        return points.stream()
-          .filter(point -> point.getX() < 0 && point.getY() < 0)
-          .distinct()
-          .collect(Collectors.toList());
+        return pointFilter(points, point -> point.getX() < 0 && point.getY() < 0);
     }
 
     public List<Point> getUniquePointsInQuadrantFour(List<Point> points) {
-        return points.stream()
-          .filter(point -> point.getX() > 0 && point.getY() < 0)
-          .distinct()
-          .collect(Collectors.toList());
+        return pointFilter(points, point -> point.getX() > 0 && point.getY() < 0);
     }
 
     public List<Double> getSortedPointsDistanceToOrigin(List<Point> points) {
@@ -45,6 +34,13 @@ public class Streams {
         return points.stream()
           .map(point -> getDistanceToOrigin(point))
           .reduce(0.0, (point1, point2) -> (point1) + (point2));
+    }
+
+    public List<Point> pointFilter(List<Point> points, Predicate<Point> predicate) {
+        return points.stream()
+          .filter(predicate)
+          .distinct()
+          .collect(Collectors.toList());
     }
 
     public Double getDistanceToOrigin(Point point) {
